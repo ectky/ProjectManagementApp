583d7e5830b04566b07eb48d034f1c922ef7aa2b
Ivo Mihailov <ivomihailov2006@gmail.com>
2024-05-25 11:31:50 +0300

    fixed TaskDto

--- ProjectManagement.Shared/Dtos/TaskDto.cs
+++ ProjectManagement.Shared/Dtos/TaskDto.cs
@@ -10,17 +10,17 @@ namespace ProjectManagement.Shared.Dtos
 {
     public class TaskDto : BaseModel
     {
+        public TaskDto()
+        {
+            this.Users = new List<UserDto>();
+        }
         public string Description { get; set; }
         public string Name { get; set; }
         public Status Status { get; set; }
         public DateTime? Deadline { get; set; }
 
         public int ProjectId { get; set; }
-        public TaskDto()
-        {
-            this.Users = new List<UserDto>();
-        }
-        public virtual ProjectDto Project { get; set; }
-        public virtual ICollection<UserDto> Users { get; set; } = new List<UserDto>();
+        public ProjectDto Project { get; set; }
+        public ICollection<UserDto> Users { get; set; } = new List<UserDto>();
     }
 }
