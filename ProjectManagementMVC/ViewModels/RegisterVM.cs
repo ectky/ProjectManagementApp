06024f0b05972d02b89a85ca0694461ddf1a213e
Petkata23 <152802930+Petkata23@users.noreply.github.com>
2024-06-03 08:05:21 +0300

    Updated RegisterVM.cs

--- ProjectManagementMVC/ViewModels/RegisterVM.cs
+++ ProjectManagementMVC/ViewModels/RegisterVM.cs
@@ -20,10 +20,6 @@ namespace ProjectManagementMVC.ViewModels
 
         [Required(ErrorMessage = "First name is required")]
         public string FirstName { get; set; }
-
-        [Required(ErrorMessage = "Role is required")]
-        public int RoleId { get; set; }
-
-        public int? ProjectId { get; set; }
+ 
     }
 }
