6e4fb36452b54cf162e5c469583334b40ae97a0a
unknown <petarh06@gmail.com>
2024-06-05 10:33:45 +0300

    Auth work in progress

--- ProjectManagementMVC/Program.cs
+++ ProjectManagementMVC/Program.cs
@@ -1,6 +1,14 @@
 using System.Configuration;
+using EntityFrameworkCore.UseRowNumberForPaging;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
+using NuGet.Configuration;
+using ProjectManagament.Services;
 using ProjectManagement.Data;
+using ProjectManagement.Data.Repos;
+using ProjectManagement.Shared.Services.Contracts;
+using ProjectManagementMVC;
+using ProjectManagement.Shared.Extensions;
 
 
 
@@ -8,12 +16,31 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAutoMapper(m => m.AddProfile(new AutoMapperConfiguration()));
+builder.Services.AutoBind(typeof(ProjectsService).Assembly);
+builder.Services.AutoBind(typeof(ProjectRepository).Assembly);
+
+// TODO   add more 
+
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie();
+
 
 builder.Services.AddDbContext<ProjectManagementDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+{
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnection"], i => i.UseRowNumberForPaging());
+});
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<ProjectManagementDbContext>();
+    // Automatically update database
+    context.Database.Migrate();
+}
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
